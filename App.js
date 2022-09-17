7e0f943ed01073c789863111ca67da3e0017a628
mr_sd_jack_003 <91643563+dalalsoham@users.noreply.github.com>
2022-09-17 14:06:22 +0530

    2nd commit JS files and Css files

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import header from "./My components/header";
+
+function App() {
+  let myVerable = 345;
+  return (
+
+    <>
+    <header/>
+    <Todos/>
+    </>
+  );
+}
+
+export default App;
